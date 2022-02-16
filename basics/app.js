95f25e3bf923a47f01b43bb70c512fb2473c1cab
Danush Kumar <danushkumar27@gmail.com>
2022-02-16 11:25:47 +0530

    Raw routing of request and response

--- basics/app.js
+++ basics/app.js
@@ -1,5 +1,7 @@
 //Importing http library
 const http = require("http");
+const fs = require("fs");
+const { Console } = require("console");
 
 //Creating a server using the library
 const server = http.createServer((req, res) => {
@@ -8,7 +10,40 @@ const server = http.createServer((req, res) => {
   //Printing specific parts from request object which is sent to server
   console.log(req.headers, req.method);
   //process.exit();
+  //Parsing URL and method data from req object
+  const url = req.url;
+  const method = req.method;
+  //Routing
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Messenger</title></head>");
+    res.write(
+      '<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
+  //Dealing with request
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    //Getting data from url to interact with listener
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    //Listener interaction of data
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
   //Creating a response with a header
   res.setHeader("Content-Type", "text/html");
   //Creating a custom html page
