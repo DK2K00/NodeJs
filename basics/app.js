3783e313e824019cc1148cb4ceeee5977b6fd2a1
Danush Kumar <danushkumar27@gmail.com>
2022-02-19 19:50:16 +0530

    Changed file structure

--- basics/app.js
+++
@@ -1,9 +0,0 @@
-//Importing modules
-const http = require("http");
-const routes = require("./routes");
-
-//Creating a server using the library and routes.js
-const server = http.createServer(routes.handler);
-
-//Creating a server port
-server.listen(3000);
