0f877977f8e334a98ce0608ea9b3a69517cb3e05
Danush Kumar <danushkumar27@gmail.com>
2022-02-22 20:36:13 +0530

    Added handlebars files

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
+const expressHbs = require('express-handlebars');
 
 //Importing routes from different folders
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+//App engine is set due to handlebars being a 3rd party package
+//not integrated into node
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
 //Setting view engine for templates
-app.set("view engine", "pug");
+app.set("view engine", "hbs");
 //Setting default folder to find template files
 app.set("views", "views");
 
@@ -24,7 +35,7 @@ app.use(shopRoutes);
 
 //404 page
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 //Creating a server port
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
 //Importing modules
 const express = require("express");
 const router = express.Router();
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 const path = require("path");
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 //Handling routes
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   //Rendering HTML file using template engine
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
